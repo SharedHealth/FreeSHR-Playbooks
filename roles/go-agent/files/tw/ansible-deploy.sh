75f7a602a75f168678cfc91eed2f282f04b2a61e
MD Dubey <mdinfotech007@gmail.com>
2015-10-29 14:57:03 +0530

    Dubey | BDSHR-000 | Configuring Playbooks at one more place to separate out master and release deployment.

--- roles/go-agent/files/tw/ansible-deploy.sh
+++ roles/go-agent/files/tw/ansible-deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 (cd /opt/playbooks/FreeSHR-Playbooks && git pull --rebase)
 (cd /opt/playbooks/FreeSHR-Bahmni-Playbooks && git pull --rebase)
+([ -d "/opt/playbooks/development/FreeSHR-Playbooks" ] && cd /opt/playbooks/development/FreeSHR-Playbooks && git pull --rebase)
+([ -d "/opt/playbooks/development/FreeSHR-Bahmni-Playbooks" ] && cd /opt/playbooks/development/FreeSHR-Bahmni-Playbooks && git pull --rebase)
 ansible-playbook "$@"
