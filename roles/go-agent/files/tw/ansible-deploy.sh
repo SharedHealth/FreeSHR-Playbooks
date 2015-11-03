05ae715db136b7a1013fb79403031457bc837843
Sanjana Bayya <sanjanb@thoughtworks.com>
2015-11-03 15:01:28 +0530

    Sanjana | BDSHR - 1020 | ansible-deploy specific to branches.Creates individual folders for each branch

--- roles/go-agent/files/tw/ansible-deploy.sh
+++ roles/go-agent/files/tw/ansible-deploy.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-(cd /opt/playbooks/FreeSHR-Playbooks && git pull --rebase)
-(cd /opt/playbooks/FreeSHR-Bahmni-Playbooks && git pull --rebase)
-([ -d "/opt/playbooks/development/FreeSHR-Playbooks" ] && cd /opt/playbooks/development/FreeSHR-Playbooks && git pull --rebase)
-([ -d "/opt/playbooks/development/FreeSHR-Bahmni-Playbooks" ] && cd /opt/playbooks/development/FreeSHR-Bahmni-Playbooks && git pull --rebase)
-ansible-playbook "$@"
+([ -d "/opt/playbooks/FreeSHR-Playbooks/$1" ] && cd /opt/playbooks/FreeSHR-Playbooks/$1 && git checkout $1 && git pull --rebase)
+([ -d "/opt/playbooks/FreeSHR-Bahmni-Playbooks/$1" ] && cd /opt/playbooks/FreeSHR-Bahmni-Playbooks/$1 && git checkout $1 && git pull --rebase)
+ansible-playbook "${@:2}"
