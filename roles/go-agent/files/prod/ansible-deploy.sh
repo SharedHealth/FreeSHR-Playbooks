fda6d4a0082d544d9a59432862870a62dfa739a6
Sanjana Bayya <sanjanb@thoughtworks.com>
2015-11-03 17:41:24 +0530

    Sanjana | BDSHR - 1022 | Removing checking the presence of branch folders.Wanted to fail if folder not present.

--- roles/go-agent/files/prod/ansible-deploy.sh
+++ roles/go-agent/files/prod/ansible-deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 (cd /opt/playbooks/hie-config && git pull --rebase)
 (cd /opt/playbooks/FreeSHR-Playbooks/$1 && git checkout group_vars && git checkout $1 && git pull --rebase)
-(cd /opt/playbooks/FreeSHR-Bahmni-Playbooks && git checkout group_vars && git checkout $1 && git pull --rebase)
+(cd /opt/playbooks/FreeSHR-Bahmni-Playbooks/$1 && git checkout group_vars && git checkout $1 && git pull --rebase)
 cp -rf /opt/playbooks/hie-config/playbooks/* /opt/playbooks/FreeSHR-Playbooks/$1/
 cp -rf /opt/playbooks/hie-config/chw-playbooks/* /opt/playbooks/FreeSHR-Bahmni-Playbooks/$1/
 ansible-playbook "${@:2}"
--- roles/go-agent/files/tw/ansible-deploy.sh
+++ roles/go-agent/files/tw/ansible-deploy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-([ -d "/opt/playbooks/FreeSHR-Playbooks/$1" ] && cd /opt/playbooks/FreeSHR-Playbooks/$1 && git checkout $1 && git pull --rebase)
-([ -d "/opt/playbooks/FreeSHR-Bahmni-Playbooks/$1" ] && cd /opt/playbooks/FreeSHR-Bahmni-Playbooks/$1 && git checkout $1 && git pull --rebase)
+(cd /opt/playbooks/FreeSHR-Playbooks/$1 && git checkout $1 && git pull --rebase)
+(cd /opt/playbooks/FreeSHR-Bahmni-Playbooks/$1 && git checkout $1 && git pull --rebase)
 ansible-playbook "${@:2}"
