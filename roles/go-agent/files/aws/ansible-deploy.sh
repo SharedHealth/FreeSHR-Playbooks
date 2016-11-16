9b1b06593925a9d51a9b33b893e96b0d5cc42561
sukreet <sukreet001@gmail.com>
2016-11-16 12:18:55 +0530

    Sukreet | shr-1349 | fixing aws playbook branch to master for ansible deploy

--- roles/go-agent/files/aws/ansible-deploy.sh
+++ roles/go-agent/files/aws/ansible-deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 (cd /opt/playbooks/FreeSHR-Playbooks/$1 && git checkout $1 && git pull --rebase)
 (cd /opt/playbooks/FreeSHR-Bahmni-Playbooks/$1 && git checkout $1 && git pull --rebase)
-cp /opt/playbooks/SHR-AWS-Playbooks/$1/ansible.cfg /opt/playbooks/FreeSHR-Playbooks/$1/
-cp /opt/playbooks/SHR-AWS-Playbooks/$1/group_vars/shr_launch_key.pem /opt/playbooks/FreeSHR-Playbooks/$1/
+(cd /opt/playbooks/FreeSHR-Bahmni-Playbooks/master && git checkout master && git pull --rebase)
+cp /opt/playbooks/SHR-AWS-Playbooks/master/ansible.cfg /opt/playbooks/FreeSHR-Playbooks/$1/
+cp /opt/playbooks/SHR-AWS-Playbooks/master/group_vars/shr_launch_key.pem /opt/playbooks/FreeSHR-Playbooks/$1//group_vars/
 ansible-playbook "${@:2}"
