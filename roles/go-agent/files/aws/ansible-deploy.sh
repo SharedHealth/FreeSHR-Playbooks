4c43f7ec642b63045cceb2c188f6d238e0622b96
sukreet <sukreet001@gmail.com>
2016-11-16 17:24:37 +0530

    Sukreet, Neha | shr-1380 | fixed error in aws ansible deploy.

--- roles/go-agent/files/aws/ansible-deploy.sh
+++ roles/go-agent/files/aws/ansible-deploy.sh
@@ -3,5 +3,5 @@
 (cd /opt/playbooks/FreeSHR-Bahmni-Playbooks/$1 && git checkout $1 && git pull --rebase)
 (cd /opt/playbooks/SHR-AWS-Playbooks/master && git checkout master && git pull --rebase)
 cp /opt/playbooks/SHR-AWS-Playbooks/master/ansible.cfg /opt/playbooks/FreeSHR-Playbooks/$1/
-cp /opt/playbooks/SHR-AWS-Playbooks/master/group_vars/shr_launch_key.pem /opt/playbooks/FreeSHR-Playbooks/$1//group_vars/
+cp /opt/playbooks/SHR-AWS-Playbooks/master/group_vars/shr_launch_key.pem /opt/playbooks/FreeSHR-Playbooks/$1/group_vars/
 ansible-playbook "${@:2}"
